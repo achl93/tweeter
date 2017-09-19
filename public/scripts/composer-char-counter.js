8ac5f67257869427bba526b4f70f581f89f4d7fb
Andy Leung <andy_ch_009@hotmail.com>
2017-09-19 01:12:47 +0000

    Added a character counter for tweet form

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.new-tweet').find('textarea[name="text"]').keydown(function() {
+    $(this).closest('form').children('.counter').text(140 - $(this).val().length);
+    if ($(this).closest('form').children('.counter').text() < 0) {
+      $(this).closest('form').children('.counter').addClass("overCharLimit");
+    }
+    else {
+      $(this).closest('form').children('.counter').removeClass("overCharLimit");
+    }
+  });
+});
