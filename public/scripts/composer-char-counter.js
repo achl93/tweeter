7bd8ecd2239f4a9bf619f08cf24fe54bd3cb6b4b
Andy Leung <andy_ch_009@hotmail.com>
2017-09-20 20:11:29 +0000

    Adding form toggle

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.new-tweet').find('textarea[name="text"]').keydown(function() {
+  $('.new-tweet').find('textarea[name="text"]').on('keyup keydown blur change', function() {
     $(this).closest('form').children('.counter').text(140 - $(this).val().length);
     if ($(this).closest('form').children('.counter').text() < 0) {
       $(this).closest('form').children('.counter').addClass("overCharLimit");
