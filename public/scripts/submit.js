528d36e0f4d433e2eb40f74c8095a25d0dea3bc5
Andy Leung <andy_ch_009@hotmail.com>
2017-09-20 01:36:17 +0000

    Adding a check for invalid tweets

--- public/scripts/submit.js
+++ public/scripts/submit.js
@@ -1,13 +1,20 @@
 $(function() {
   let $button = $('#sendTweet');
   $button.click(function(event) {
-    event.preventDefault();
-    const tweet = $('form').serialize();
-    console.log(tweet);
-    $.ajax({
-      url: $('form').attr('action'),
-      method: 'POST',
-      data: tweet
-    })
-  });
+    if ($('.new-tweet').find('textarea[name="text"]').closest('form').children('.counter').text() < 0 || $('form') === "") {
+      console.log("Invalid");
+      event.preventDefault();
+      $(".new-tweet").append("<p style='color: red;'>Invalid Tweet</p>");
+    }
+    else {
+      console.log("Valid");
+      event.preventDefault();
+      const tweet = $('form').serialize();
+      $.ajax({
+        url: $('form').attr('action'),
+        method: 'POST',
+        data: tweet
+      })
+    }
+  })
 });
